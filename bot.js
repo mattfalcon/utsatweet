3247fe2dc9a9b1c9c6c173eaed28204fd58b31e3
matt <mattvalladarez@gmail.com>
2018-03-17 21:48:17 -0500

    update

--- bot.js
+++ bot.js
@@ -65,7 +65,7 @@ client.get('search/tweets', {q: "powerapps", count: 50}, function(error, tweets,
 
   //============POST TWEET ===========================================
   //Test case for tweeting out 
-  client.post('statuses/update', {status: "RT @" + selected_tweet.screen_name + " " + selected_tweet.statuses.text + "  Tweet Frm: " + selected_tweet.location + " #office365 @STOP_PRE"}, function(error, tweet, response) {
+  client.post('statuses/update', {status: "RT @" + selected_tweet.screen_name + " " + selected_tweet.text + "  Tweet Frm: " + selected_tweet.location + " #office365 @STOP_PRE"}, function(error, tweet, response) {
     if (!error) {
       console.log(tweet);
     }
